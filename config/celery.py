80e2a566585b7b985391cb16429ffddb791e7a50
Sharad Mishra <sharadrx9@gmail.com>
2025-07-16 04:11:32 +0530

    Fixed loading of environment variables

--- config/celery.py
+++ config/celery.py
@@ -1,12 +1,13 @@
 import os
 from celery import Celery
+from dotenv import load_dotenv
+
+load_dotenv()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('notification-service')
 
-# Load config from Django settings using CELERY_ namespace
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Autodiscover tasks from installed apps
 app.autodiscover_tasks()
